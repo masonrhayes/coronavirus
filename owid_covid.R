7146d993c8f8a65cb9a25f4f36cafbd917219252
Hayes <masonrhayes@ou.edu>
2020-05-31 22:00:53 -0500

    update README, simplify script

--- owid_covid.R
+++ owid_covid.R
@@ -4,20 +4,27 @@ library(ggthemes)
 library(lubridate)
 library(ftplottools)
 
+# Collect data set form Our World in Data
 covid_df = read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 
+# View data set
 view(covid_df)
 
 
+# List all countries of interest
+countries = c("Germany", "France", "United States", "Spain")
 
-countries = c("Italy", "Germany", "Spain", "France", "United States")
+# Set start date
+start_date = "2020-04-01"
 
+## Graph the variable(s) of interest
 covid_df %>%
   filter(location %in% countries)%>%
-  filter(date >= "2020-04-01") %>%
-  ggplot(aes(x = date, y = log(new_deaths_per_million))) +
-  geom_smooth(aes(color = location), se = FALSE) +
+  filter(date >= start_date) %>%
+  ggplot(aes(x = date, y = new_cases_per_million)) +
+  geom_line(aes(color = location), se = FALSE) +
   ft_theme() +
-  scale_color_ft()
+  scale_color_ft() +
+  labs(title = "New Cases per Million Since April 1")
 
   
